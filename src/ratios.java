1da1c682700937c0b89b10e05cfcfe955436ddaf
zhanghuimin <zhm0525@gmail.com>
2017-04-15 17:29:21 +0800

    solve the problem ratios

---
+++ src/ratios.java
@@ -0,0 +1,24 @@
+/*
+ID: zhm05251
+LANG: JAVA
+TASK: ratios
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class ratios {
+    private static final String INPUT_FILE_NAME = "ratios.in";
+    private static final String OUTPUT_FILE_NAME = "ratios.out";
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader f = new BufferedReader(new FileReader(INPUT_FILE_NAME));
+        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(OUTPUT_FILE_NAME)));
+        StringTokenizer st = new StringTokenizer(f.readLine());
+
+
+        out.println();
+        out.close();
+    }
+
+}
